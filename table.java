a5dd67468acb40a77baffbe6938936de4e2d37dc
bhavana <parasareddybhavana@gmail.com>
2026-03-13 09:15:53 +0530

    Java with DSA

--- table.java
+++
@@ -1,9 +0,0 @@
-public class table {
-    public static void main(String[] args) {
-        for(int i=1;i<=10;i++){
-            System.out.println(i+"*5="+i*5);
-        }
-        
-    }
-    
-}
